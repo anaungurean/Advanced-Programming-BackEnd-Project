c1ebbeaafb41665f98e5828f0acea52406e2133d
anaungurean <anamariaungurean01@gmail.com>
2023-05-26 19:13:34 +0300

    -add-java-project

---
+++ JavaApp/src/main/java/com/example/JavaApp/JavaAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.JavaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAppApplication.class, args);
+	}
+
+}
---
+++ JavaApp/src/test/java/com/example/JavaApp/JavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
